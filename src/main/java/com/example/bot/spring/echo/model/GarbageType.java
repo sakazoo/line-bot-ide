a8cef244351ce56542fc715ef977b06ced0da055
sakazoo <qq674gzd@gmail.com>
2020-04-03 00:47:01 +0900

    Fix message

--- src/main/java/com/example/bot/spring/echo/model/GarbageType.java
+++ src/main/java/com/example/bot/spring/echo/model/GarbageType.java
@@ -3,11 +3,18 @@ package com.example.bot.spring.echo.model;
 import java.time.DayOfWeek;
 
 public enum GarbageType {
-
-  BURNABLE(new DayOfWeek[]{DayOfWeek.TUESDAY, DayOfWeek.FRIDAY}, "燃えるゴミ"),
-  RESOURCE(new DayOfWeek[]{DayOfWeek.WEDNESDAY}, "資源ゴミ"),
-  INCOMBUSTIBLE(new DayOfWeek[]{DayOfWeek.THURSDAY}, "燃えないゴミ"),
-  NONE(new DayOfWeek[]{DayOfWeek.MONDAY, DayOfWeek.SATURDAY, DayOfWeek.SUNDAY}, "通常日");
+  BURNABLE(new DayOfWeek[] {DayOfWeek.TUESDAY, DayOfWeek.FRIDAY}, "明日は燃えるゴミなのでヨシ！"),
+  RESOURCE(
+      new DayOfWeek[] {DayOfWeek.WEDNESDAY},
+      "明日は資源ゴミなのでヨシ！\n"
+          + "ダンボール、ペットボトル、空き缶空き瓶を捨てるヨシ！\n"
+          + "詳しくはこちらを確認↓\n"
+          + "https://www.city.shinagawa.tokyo.jp/PC/kankyo/kankyo-gomi/kankyo-gomi-dashikata/hpg000005606.html"),
+  INCOMBUSTIBLE(new DayOfWeek[] {DayOfWeek.THURSDAY}, "明日は燃えないゴミなのでヨシ！\n" +
+          "陶器・ガラス、金属類を捨てるヨシ！\n" +
+          "詳しくはこちらを確認↓\n" +
+          "https://www.city.shinagawa.tokyo.jp/PC/kankyo/kankyo-gomi/kankyo-gomi-dashikata/hpg000005605.html"),
+  NONE(new DayOfWeek[] {DayOfWeek.MONDAY, DayOfWeek.SATURDAY, DayOfWeek.SUNDAY}, "通常日");
 
   private DayOfWeek[] weeks;
 
