96312255b182852893cb9ba8b5589753b0f75986
sakazoo <qq674gzd@gmail.com>
2020-04-04 10:45:30 +0900

    Refactoring create event class

--- src/main/java/com/example/bot/spring/echo/EchoApplication.java
+++ src/main/java/com/example/bot/spring/echo/EchoApplication.java
@@ -1,15 +1,8 @@
 package com.example.bot.spring.echo;
 
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.linecorp.bot.model.event.Event;
-import com.linecorp.bot.model.event.MessageEvent;
-import com.linecorp.bot.model.event.message.TextMessageContent;
-import com.linecorp.bot.model.message.Message;
-import com.linecorp.bot.model.message.TextMessage;
-import com.linecorp.bot.spring.boot.annotation.EventMapping;
-import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
@@ -18,17 +11,5 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class EchoApplication {
   public static void main(String[] args) {
     SpringApplication.run(EchoApplication.class, args);
-  }
-
-  @EventMapping
-  public Message handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
-    System.out.println("event: " + event);
-    final String originalMessageText = event.getMessage().getText();
-    return new TextMessage(originalMessageText);
-  }
-
-  @EventMapping
-  public void handleDefaultMessageEvent(Event event) {
-    System.out.println("event: " + event);
   }
 }
