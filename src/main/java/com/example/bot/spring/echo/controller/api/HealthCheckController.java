906323eb0dbe89d20d1100d51cfdde29f0cd4012
sakazoo <qq674gzd@gmail.com>
2020-05-12 22:22:06 +0900

    Add mapping

--- src/main/java/com/example/bot/spring/echo/controller/api/HealthCheckController.java
+++ src/main/java/com/example/bot/spring/echo/controller/api/HealthCheckController.java
@@ -1,4 +1,13 @@
 package com.example.bot.spring.echo.controller.api;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HealthCheckController {
+  @GetMapping("/health-check")
+  public ResponseEntity<String> status(){
+    return ResponseEntity.ok("running");
+  }
 }
